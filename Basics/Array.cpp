99687544343bb96f1edd37b02f4d37188d363d0c
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2023-12-09 12:39:18 +0530

    Changes

---
+++ Basics/Array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[4] = {1, 2, 3, 4};
+
+    cout << "The array memory location is: " << arr << endl;
+    cout << "The array value of 0th position is : " << arr[4] << endl;
+
+    int arr1[4];
+    arr1[0] = 10;
+    arr1[1] = 20;
+
+    cout << "The array memory location is: " << arr1 << endl;
+    cout << "The array value of 1st position is : " << arr1[1] << endl;
+
+    *arr = 5;
+    cout << "The array value of 0th position is : " << arr[8] << endl;
+
+    return 0;
+}
