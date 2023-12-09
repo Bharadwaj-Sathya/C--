5082ae32f1a7085ab34a568cadf9f75cc441f617
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2023-12-09 13:04:32 +0530

    Changes in filesa

--- Basics/Array.cpp
+++ Basics/Array.cpp
@@ -6,7 +6,7 @@ int main()
     int arr[4] = {1, 2, 3, 4};
 
     cout << "The array memory location is: " << arr << endl;
-    cout << "The array value of 0th position is : " << arr[4] << endl;
+    cout << "The array value of 0th position is : " << arr[0] << endl;
 
     int arr1[4];
     arr1[0] = 10;
@@ -16,7 +16,14 @@ int main()
     cout << "The array value of 1st position is : " << arr1[1] << endl;
 
     *arr = 5;
-    cout << "The array value of 0th position is : " << arr[8] << endl;
+    cout << "The array value of 0th position is : " << arr[0] << endl;
+
+    int *arr2 = arr1;
+    arr2++;
+
+    *arr2 = 209;
+
+    cout << "The array value of 1st position is : " << arr1[1] << endl;
 
     return 0;
 }
--- Basics/VariablesDataTypes.cpp
+++ Basics/VariablesDataTypes.cpp
@@ -5,14 +5,17 @@ using namespace std;
 int main()
 {
     cout << "------------Data Types Primitive------------ \n";
-    cout << "Type                         Keyweord\n";
-    cout << "Boolean                      bool \n";
-    cout << "Character                    char \n";
-    cout << "integer                      int  \n";
-    cout << "Floating point               float \n";
-    cout << "Double Floating point        double \n";
-    cout << "Valueless                    void \n";
-    cout << "------------Data Types Primitive------------ \n";
+    cout << "Type                         Keyweord                        Size\n";
+    cout << "Boolean                      bool                           " << sizeof(bool) << endl;
+    cout << "Character                    char                           " << sizeof(char) << endl;
+    cout << "Integer                      int                            " << sizeof(int) * 8 << endl;
+    cout << "Short Integer                short int                      " << sizeof(short int) * 8 << endl;
+    cout << "Long Integer                 long int                       " << sizeof(long int) * 8 << endl;
+    cout << "Long Long Integer            long long int                  " << sizeof(long long int) * 8 << endl;
+    cout << "Floating point               float                          " << sizeof(float) * 8 << endl;
+    cout << "Double Floating point        double                         " << sizeof(double) * 8 << endl;
+    cout << "Valueless                    void                           " << sizeof(0) << endl;
+    cout << "------------Data Types Primitive------------- \n";
 
     string str;
     cout << "Enter your string: ";
