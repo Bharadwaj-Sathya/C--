b3cfd2f7ffc2588589349990cbda4328ad56fe85
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2023-12-09 18:59:58 +0530

    Changes

---
+++ Basics/Conditionals.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num = 10;
+    int num1 = 10;
+
+    if (num > num1)
+    {
+        cout << "num is greater than num1" << endl;
+    }
+    else if (num1 > num)
+    {
+        cout << "num1 is greater than num" << endl;
+    }
+    else
+    {
+        cout << "Both are equal" << endl;
+    }
+    return 0;
+}
