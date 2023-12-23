cfbfe23b24cc5c7eef1b8e87b18bd49db734029c
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2023-12-23 11:14:01 +0530

    Changes in Exponent recursion

--- DataStructures&Algorithms/Recursion/ExponentRecursion.cpp
+++ DataStructures&Algorithms/Recursion/ExponentRecursion.cpp
@@ -12,11 +12,26 @@ int pow(int m, int n)
         return pow(m, n - 1) * m;
     }
 }
+
+int power(int m, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    if (n % 2 == 0)
+    {
+        return power(m * m, n / 2);
+    }
+    return m * power(m * m, (n - 1) / 2);
+}
 int main()
 {
     int i = 0;
     int n = 5;
 
-    cout << pow(2, 9);
+    cout << pow(2, 9) << endl;
+    cout << power(2, 9) << endl;
+
     return 0;
 }
