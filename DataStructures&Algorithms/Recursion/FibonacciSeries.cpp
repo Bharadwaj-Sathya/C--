0b7cb7305a9ca849d40da5a77523db8a4cd5b269
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2023-12-23 18:56:58 +0530

    Reducing recursive calls by using memorisation method

--- DataStructures&Algorithms/Recursion/FibonacciSeries.cpp
+++ DataStructures&Algorithms/Recursion/FibonacciSeries.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+int F[10];
+
 int fibonacci(int n)
 {
     if (n <= 1)
@@ -13,6 +15,7 @@ int fibonacci(int n)
     }
 }
 
+// Excessive Recursion
 int fibonacciIterative(int n)
 {
     if (n <= 1)
@@ -34,11 +37,40 @@ int fibonacciIterative(int n)
         return sum;
     }
 }
+
+int fibonacciMemorisation(int n)
+{
+    if (n <= 1)
+    {
+        F[n] = n;
+        return n;
+    }
+    else
+    {
+        if (F[n - 2] == -1)
+        {
+            F[n - 2] = fibonacciMemorisation(n - 2);
+        }
+        if (F[n - 1] == -1)
+        {
+            F[n - 1] = fibonacciMemorisation(n - 1);
+        }
+
+        return F[n - 2] + F[n - 1];
+    }
+}
+
 int main()
 {
     int x = 6;
     cout << "Fibbonacci Series: of " << x << " is: " << fibonacci(x) << endl;
     cout << "Fibbonacci Series: of " << x << " is: " << fibonacciIterative(x) << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        F[i] = -1;
+    }
+    cout << "Fibbonacci Series: of " << x << " is: " << fibonacciMemorisation(x) << endl;
 
     return 0;
 }
