d1bf8000a42cd92c6c4a896f3acdd4623b2bae8b
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2023-12-23 14:51:50 +0530

    Changes

--- DataStructures&Algorithms/Recursion/TailorSeriesUsingRecursion.cpp
+++ DataStructures&Algorithms/Recursion/TailorSeriesUsingRecursion.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
+double tailorSeries(int x, int n)
+{
+    static double p = 1, f = 1;
+    double r;
+
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        r = tailorSeries(x, n - 1);
+        p = p * x;
+        f = f * n;
+        return r + p / f;
+    }
+}
 int main()
 {
-
+    int x = 4;
+    int y = 15;
+    cout << tailorSeries(x, y) << endl;
     return 0;
 }
