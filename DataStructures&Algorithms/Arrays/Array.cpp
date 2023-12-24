6745fe2edcf32b09b976b916525ca1a5c7451859
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2023-12-24 21:51:00 +0530

    Changes

--- DataStructures&Algorithms/Arrays/Array.cpp
+++ DataStructures&Algorithms/Arrays/Array.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 using namespace std;
 
-
-
 int main()
 {
+    // Creating array in stack
+    int a[5] = {2, 4, 6, 8, 9};
 
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << endl;
+    }
+
+    // Creating array in heap
+    int *p = new int[10];
+    cout << "Elements in P:" << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        p[i] = i;
+        cout << p[i] << endl;
+    }
+    cout << "Elements in q:" << endl;
+
+    int *q = new int[15];
+
+    for (int i = 0; i < 10; i++)
+    {
+        q[i] = p[i];
+    }
+
+    for (int i = 0; i < 15; i++)
+    {
+
+        cout << q[i] << endl;
+    }
+
+    delete[] p;
+    p = q;
+    q = NULL;
     return 0;
 }
---
+++ DataStructures&Algorithms/Arrays/TwoDArray.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int a[3][4];
+
+    return 0;
+}
