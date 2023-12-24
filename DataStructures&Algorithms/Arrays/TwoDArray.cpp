450c4e24b7f5e06c201358b9bb110a378db10186
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2023-12-24 21:59:27 +0530

    Changes

--- DataStructures&Algorithms/Arrays/TwoDArray.cpp
+++ DataStructures&Algorithms/Arrays/TwoDArray.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    int a[3][4];
+    // Declarinng and intializing
+    int a[3][4] = {{1, 2, 3, 4}, {2, 4, 6, 8}, {1, 3, 5, 7}};
+
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cout << a[row][col];
+        }
+        cout << "\n";
+    }
 
     return 0;
 }
